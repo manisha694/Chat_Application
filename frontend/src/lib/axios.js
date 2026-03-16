45e8792c932bba0c497ddd9285bbce476e24c804
ManishaChaurasiya <manishachaurasiya694@gmail.com>
2026-03-16 12:18:23 +0530

    connect frontend to render backend

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,11 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
+  baseURL: import.meta.env.MODE === "development"
+    ? "http://localhost:5001/api"
+    : "https://chat-application-8ws8.onrender.com/api",
   withCredentials: true,
 });
+
+
+
